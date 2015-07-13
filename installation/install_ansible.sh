5c3f606f51db9449ab5487615fd1fccc2f82aabb
niv <niv.goor@polymont.fr>
2015-07-13 17:04:31 +0200

    added pre-requisites for installing ansible (ssh) and for using docker_image module (docker-py)

--- installation/install_ansible.sh
+++ installation/install_ansible.sh
@@ -5,6 +5,9 @@
 #First add the EPEL repository:
 rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 
+# verify ssh installed
+yum install -y openssh-clients
+
 #then run
 yum install -y ansible
 
