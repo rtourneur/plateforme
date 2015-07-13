a7e116e0df56d24a7a22cfb81eeccadf900c1d49
niv <niv.goor@polymont.fr>
2015-07-13 16:31:17 +0200

    added docker installation playbook

---
+++ installation/install_ansible.sh
@@ -0,0 +1,11 @@
+#Execute: sudo bash install_ansible.sh
+
+#(Pre-req: python >= 2.7)
+
+#First add the EPEL repository:
+rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+
+#then run
+yum install -y ansible
+
+# set ansible/hosts file
