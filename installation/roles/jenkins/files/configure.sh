66acbec9c8802fa9bb3427c3843f2d31e353cda7
Raphael Tourneur <raphael.tourneur@polymont.fr>
2015-08-11 15:16:11 +0200

    Intégration Git dans Jenkins

---
+++ installation/roles/jenkins/files/configure.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Configure Maven
+script=`cat maven.groovy`
+curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
+
+# Configure Git
+script=`cat git.groovy`
+curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
--- installation/roles/jenkins/files/maven.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-script=`cat maven.groovy`
-curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
