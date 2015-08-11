31655b029e8a5f16fd22b48463cd0d294ce09e6b
Raphael Tourneur <raphael.tourneur@polymont.fr>
2015-08-11 15:38:47 +0200

    Intégration Java dans Jenkins

--- installation/roles/jenkins/files/configure.sh
+++ installation/roles/jenkins/files/configure.sh
@@ -7,3 +7,7 @@ curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
 # Configure Git
 script=`cat git.groovy`
 curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
+
+# Configure Java
+script=`cat jdk.groovy`
+curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
