0a1c0f10e7a46577d342373ca69391d4d4908b3a
Raphael Tourneur <raphael.tourneur@polymont.fr>
2015-08-12 11:54:06 +0200

    Configuration de la sécurité dans Jenkins

--- installation/roles/jenkins/files/configure.sh
+++ installation/roles/jenkins/files/configure.sh
@@ -1,13 +1,31 @@
 #! /bin/bash
 
-# Configure Maven
-script=`cat maven.groovy`
+# Appelle curl en spécifiant l'utilisateur et en vérifiant la réponse de Jenkins
+# arg1 : le script groovy
+call() {
+  file=$1
+  script=`cat $file`
+  curl -u admin:$password -d "script=$script" -o $file.log http://127.0.0.1:9080/jenkins/scriptText
+  
+  if [ -s $file.log ];
+  then
+    echo "erreur exécution script "$file
+    rm $file.log
+    exit 1
+  fi
+}
+
+password=$1
+
+# Configure security
+script=`cat admin.groovy`
 curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
+
+# Configure Maven
+call maven.groovy
 
 # Configure Git
-script=`cat git.groovy`
-curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
+call git.groovy
 
 # Configure Java
-script=`cat jdk.groovy`
-curl -d "script=$script" http://127.0.0.1:9080/jenkins/scriptText
+call jdk.groovy
