7a2252f437b28ff9d38d49349c04a3b927638512
Raphael Tourneur <raphael.tourneur@polymont.fr>
2015-08-13 15:41:33 +0200

    Lancement du service SSH dans Jenkins et Gitblit

--- installation/roles/jenkins/files/jenkins.sh
+++ installation/roles/jenkins/files/jenkins.sh
@@ -17,12 +17,14 @@ copy_reference_file() {
 		cp -r /usr/share/jenkins/ref/${rel} /var/jenkins_home/${rel}; 
 	fi; 
 }
+
+echo 
 export -f copy_reference_file
 find /usr/share/jenkins/ref/ -type f -exec bash -c 'copy_reference_file {}' \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-   catalina.sh run 
+   $CATALINA_HOME/bin/catalina.sh run 
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
