52f393a0f350e97e5674d7656dbf4c4366cdea63
Raphael Tourneur <raphael.tourneur@polymont.fr>
2015-08-25 15:11:26 +0200

    Script de configuration pour Gitblit

---
+++ installation/roles/gitblit/files/configure.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+password=$1
+
+# Install powertools plugins
+sshpass -p admin ssh admin@localhost -p 29418 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null plugin install powertools
+
+# Change admin password
+sshpass -p admin ssh admin@localhost -p 29418 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null gitblit users set admin password $password
+
+# Verify plugins lists
+sshpass -p $password ssh admin@localhost -p 29418 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null plugin list > plugins.log
+if grep -q "powertools" gitblit.log; then
+   rm plugins.log
+ fi
+
+#Get list of teams
+sshpass -p $password ssh admin@localhost -p 29418 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null gitblit teams ls > teams.log
+if grep -q "developers" teams.log; then
+   rm teams.log
+else
+  # Create developers teams
+  sshpass -p $password ssh admin@localhost -p 29418 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null gitblit teams new developers --canFork --canCreate > teams.log
+  if grep -q "developers" teams.log; then
+     rm teams.log
+  else
+    exit
+  fi
+fi
+
+# add users to team
+for i in {1..4}; do 
+  sshpass -p $password ssh admin@localhost -p 29418 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null gitblit teams members developers user$i  > teams.log
+  if grep -q "user$i" teams.log; then
+     rm teams.log
+  else
+    exit
+  fi
+done
