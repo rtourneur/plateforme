034b14fd51d315bcc85032ca3abdedb0df122952
ABDALLAH.BENBRAHIM <abdallah.benbrahim@polymont.fr>
2015-09-03 14:39:28 +0200

    script de création de projet dans jenkins

---
+++ logitheque/groovy/creation_projet_jenkins.groovy
@@ -0,0 +1,50 @@
+import hudson.maven.MavenModuleSet
+import hudson.model.JDK
+import jenkins.model.Jenkins
+
+import com.cloudbees.plugins.credentials.CredentialsScope
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider
+import com.cloudbees.plugins.credentials.domains.Domain
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl
+
+
+def instance = Jenkins.getInstance()
+MavenModuleSet mavenModuleSet = instance.createProject(MavenModuleSet.class,"{job}");
+mavenModuleSet.setRunHeadless( true );
+
+//def jdk = new JDK("JDK 8",System.getProperty("java.home"))
+def jdk = new JDK("{jdk}",null)
+mavenModuleSet.setJDK(jdk)
+
+mavenModuleSet.setMaven("{maven}")
+mavenModuleSet.setRootPOM("pom.xml")
+mavenModuleSet.setGoals("{goals}")
+
+global_domain = Domain.global()
+credentials_store = SystemCredentialsProvider.getInstance().getStore();
+
+list = credentials_store.getCredentials(global_domain)
+
+def credentials=null 
+for (item in list){	
+	if (item instanceof UsernamePasswordCredentialsImpl && "admin".equals(((UsernamePasswordCredentialsImpl)item).getUsername())) {
+		credentials = item	
+	}	 
+}
+
+if (credentials == null) {  
+	credentials = new UsernamePasswordCredentialsImpl(
+			CredentialsScope.GLOBAL,
+			null,
+			"Utilisateur pour GitBlit",
+			"admin",
+			"{password}",
+			)
+	credentials_store.addCredentials(global_domain, credentials)
+}
+
+def scm = new hudson.plugins.git.GitSCM("{giturl}")
+scm.userRemoteConfigs[0].credentialsId = credentials.getId()
+
+mavenModuleSet.setScm(scm)
+instance.save();
---
+++ logitheque/scripts/creation_project_jenkins.sh
@@ -0,0 +1,115 @@
+#! /bin/bash
+
+#################################################################################################################################################################
+#Execute: creation_projet_jenkins.sh
+
+function usage
+{
+    echo "Usage : creation_projet_jenkins.sh [[[-u USER ] [-n PROJECT_NAME] [-j JDK_VERSION] [-m MAVEN_VERSION] [-g GOALS_VALUE] [] | [-h]]"
+    echo ""
+    echo "Options :"
+    echo "  -u USER,          --user USER                    Jenkins valid user"
+    echo "  -j JDK_VERSION,   --jdk-version JDK_VERSION      JDK version"
+    echo "  -m MAVEN_VERSION, --maven-version MAVEN_VERSION  Maven version"
+    echo "  -g GOALS_VALUE,   --goals-value GOALS_VALUE      goals value"
+    echo "  -a APP_NAME,      --application APP_NAME         Application name" 
+    echo "  -p PROJECT_NAME,  --project PROJECT_NAME         Project name" 
+    echo "  -b PATH,          --base PATH                    Base folder for project"
+    echo "  -h,               --help                         show this help message and exit"
+}
+ 
+
+##### Main
+echo "Vérification des paramètres"
+
+user=
+appname=
+projectname=
+
+if [ $# -eq 0 ]; then
+  usage
+  exit
+fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -u | --user )           shift
+                                user=$1
+                                ;;
+        -j | --jdk-version )    shift
+                                jdkversion=$1
+                                ;;
+        -m | --maven-version )  shift
+                                mavenversion=$1
+                                ;;
+        -g | --goals-value )    shift
+                                mavengoals=$1
+                                ;;
+        -a | --application )    shift
+                                appname=$1
+                                ;;
+        -p | --project )        shift
+                                projectname=$1
+                                ;;
+        -b | --base )           shift
+                                basefolder=$1
+                                ;;                              
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+if [[ -z "$user" || -z "$appname" || -z "$projectname" ]]; then
+  usage
+  exit
+fi
+
+if [[ -z "$jdkversion" ]]; then
+  jdkversion='JDK 8'
+fi
+
+if [[ -z "$mavenversion" ]]; then
+  mavenversion='MAVEN 3.3'
+fi
+
+if [[ -z "$mavengoals" ]]; then
+  mavengoals='clean install'
+fi
+
+GITBLIT_HOST=`cat ~/gitblit_host`
+
+repository=$projectname/$appname
+if [ "$basefolder" ]; then
+  repository=$basefolder/$repository
+fi
+
+giturl=git://$GITBLIT_HOST/$repository.git
+
+read -s -p "Enter Password: " mypassword
+
+#################################################################################################################################################################
+# Appelle curl en specifiant l'utilisateur et en verifiant la reponse de Jenkins
+# arg1 : le script groovy
+  file=creation_projet_jenkins.groovy
+
+# remplacer les parametres
+sed -e 's/${job}/$projectname/' \
+    -e 's/${jdk}/$jdkversion/'  \
+    -e 's/${maven}/$mavenversion/' \
+    -e 's/${goals}/$mavengoals/' \
+    -e 's/${giturl}/$giturl/' \
+        $file > /tmp/file.tmp
+
+script=`cat /tmp/file.tmp`
+ 
+curl -u $user:$password -d "script=$script" -o $file.log https://$GITBLIT_HOST/jenkins/scriptText -k
+  
+if [ -s $file.log ];
+then
+  echo "erreur exécution script "$file
+  exit 1
+fi
