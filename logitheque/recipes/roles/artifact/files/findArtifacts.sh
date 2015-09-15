71d939c82e1741db9abc41e8d84654d9a0026a54
Christophe Scarfogliere <c.scarfogliere@novia-systems.fr>
2015-09-15 18:12:15 +0200

    Nettoyage et commentaires du script d'identification des artefacts tomcat

--- logitheque/recipes/roles/artifact/files/findArtifacts.sh
+++ logitheque/recipes/roles/artifact/files/findArtifacts.sh
@@ -1,40 +1,29 @@
 #! /bin/bash
 # Build the list of artifacts in yaml list format
 
-#parameter : project_home_dir
+#parameter : project_home_dir - the directory of the project, where we search artifacts
 project_home_dir=$1
-#parameter : artifact filetype
+#parameter : artifact filetype - the file type extension that let us know a file is an artifact
 artifactExtension=$2
-#parameter : targetFileName
+#parameter : targetFileName - the file where to output the results, in Yaml format
 targetFileName=$3
 
-#! /bin/bash
-# Build the list of artifacts in yaml list format
-
-#parameter : project_home_dir
-project_home_dir=$1
-#parameter : artifact filetype
-artifactExtension=$2
-#parameter : targetFileName
-targetFileName=$3
-
-#init
+#init : reset output
 rm -f $targetFileName
 
-# get artifacts filenames
-# find /opt/application01/ -name *.xml -fprint ~/tmp/test2
-find $project_home_dir -name *.$artifactExtension -fprint ~/tmp/test2
-
-# loop on file names
+# get artifacts filenames, loop on
+# TODO filter on target directories only
 for FILENAME in `find $project_home_dir -name *.$artifactExtension -print`
 do
   echo '- "'$FILENAME'"' >> $targetFileName.work
-#  echo '- "'$FILENAME'"'
 done
 
+# layout result in Yaml format (a list in a 'files' node)
 if [[ -e $targetFileName.work ]] ; then
   echo '---' > $targetFileName
   echo 'files:' >> $targetFileName
   cat $targetFileName.work >>$targetFileName
 fi
+
+#cleanup
 rm -f $targetFileName.work
